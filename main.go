5a7b59fe2e54ba48b1490c3b060d273921d95ace
kibonga <pavledjurdjic30@gmail.com>
2023-10-20 22:42:38 +0200

    Working solution for summing two numbers with binary operations

--- main.go
+++ main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+func getSum(a, b int) int {
+
+	for b != 0 {
+		temp := (a & b) << 1
+		a = (a ^ b)
+		b = temp
+	}
+
+	return a
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	a := 2
+	b := 3
+
+	result := getSum(a, b)
+	fmt.Println("Result = ", result)
 }
