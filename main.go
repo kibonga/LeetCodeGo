262862a7ccee702db0e85b3aacf048691caaf35b
kibonga <pavledjurdjic30@gmail.com>
2023-10-22 20:52:58 +0200

    Working solution for finding number of 1 bits in uint

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+func hammingWeight(num uint32) int {
+	res := 0
+	for num > 0 {
+		num &= num - 1
+		res += 1
+	}
+	return res
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	var x uint32 = 5
+	res := hammingWeight(x)
+	fmt.Println("Number of 1 bits = ", res)
 }
