5d5a73e0186a0b1caba13ae3e1c15adfe17aecbe
kibonga <pavledjurdjic30@gmail.com>
2023-10-23 22:57:23 +0200

    Working solution for finding single number in int[]

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+func singleNumber(nums []int) int {
+	res := 0
+
+	for _, num := range nums {
+		res ^= num
+	}
+
+	return res
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	var nums = []int{4, 1, 2, 1, 2}
+
+	res := singleNumber(nums)
+	fmt.Println("Result = ", res)
 }
