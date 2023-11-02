7f6e5648298e05d0ac7f49a5064928635d71b970
kibonga <pavledjurdjic30@gmail.com>
2023-11-02 22:55:58 +0100

    Copy pasta solution for subsets II

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
+
+func subsets(nums []int) [][]int {
+	n := len(nums)
+	ans := make([][]int, 0, 1<<n)
+	curr := make([]int, 0, n)
+	sort.Ints(nums)
+	var dfs func(idx int)
+
+	dfs = func(idx int) {
+		ans = append(ans, append([]int{}, curr...))
+		for i := idx; i < n; i++ {
+			if i > idx && nums[i] == nums[i-1] {
+				continue
+			}
+			curr = append(curr, nums[i])
+			dfs(i + 1)
+			curr = curr[:len(curr)-1]
+		}
+	}
+
+	dfs(0)
+	return ans
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	nums := []int{1, 2, 2}
+	res := subsets(nums)
+
+	for _, re := range res {
+		if len(re) == 0 {
+			continue
+		}
+		fmt.Print("[ ")
+		for _, r := range re {
+			fmt.Print(r, " ")
+		}
+		fmt.Println(" ]")
+	}
 }
